3a7aeedbaa6b4e381aa269b913d72686e6a670f3
Thana <kookza01@gmail.com>
2023-02-03 20:33:55 +0700

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import Button from '@mui/material/Button';
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import Button from "@mui/material/Button"
 
 function App() {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
-	return (
-		<div className='App'>
-			<div>
-				<a href='https://vitejs.dev' target='_blank'>
-					<img src='/vite.svg' className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://reactjs.org' target='_blank'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-				<Button variant='contained'>Hello World</Button>
-			</div>
-			<h1 className='bg-red-300 text-red-300'>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <Button variant="contained">Hello World</Button>
+      </div>
+      <h1 className="bg-red-300 text-red-300">Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
 }
 
-export default App;
+export default App
