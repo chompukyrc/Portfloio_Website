1611a3d11e2fe469592a0f81957d4b3087268e1d
chompukyrc <64010037@kmitl.ac.th>
2023-05-18 15:58:22 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,66 @@ import reactLogo from "./assets/react.svg"
 import Button from "@mui/material/Button"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <Button variant="contained">Hello World</Button>
+      {/* navbar */}
+      <div className="flex w-full justify-between z-50 absolute bg-white bg-opacity-50">
+        <div>
+          <ul>
+            <li
+              className="mx-12 my-4 text-4xl font-extrabold text-black hover:cursor-pointer hover:scale-105"
+              href="#"
+            >
+              LOGO
+            </li>
+          </ul>
+        </div>
+        <div>
+          <ul className="flex mr-8">
+            <li
+              className="my-4 mx-12 text-2xl font-bold text-black hover:cursor-pointer hover:scale-105"
+              href="#"
+            >
+              Home
+            </li>
+            <li
+              className="my-4 mx-12 text-2xl font-bold text-black hover:cursor-pointer hover:scale-105"
+              href="#"
+            >
+              About
+            </li>
+            <li
+              className="my-4 mx-12 text-2xl font-bold text-black hover:cursor-pointer hover:scale-105"
+              href="#"
+            >
+              Work
+            </li>
+            <li
+              className="my-4 mx-12 text-2xl font-bold text-black hover:cursor-pointer hover:scale-105"
+              href="#"
+            >
+              Contact
+            </li>
+          </ul>
+        </div>
       </div>
-      <h1 className="bg-red-300 text-red-300">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <img
+        src="\src\assets\Image\moutain.png"
+        alt="moutain"
+        className="absolute w-screen"
+      ></img>
+      <img
+        src="\src\assets\Image\forest.png"
+        alt="forest"
+        className="absolute w-screen z-10"
+      ></img>
+      <div className="text-8xl font-extrabold text-white absolute flex w-full h-screen justify-center items-center z-9">
+        KANYARAT CHAIYANBOON
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <img src="\src\assets\Image\sun.png" alt="sun" className=" w-screen"></img>
+      <div className="bg-[#7eb837] h-screen pt-12 text-center font-extrabold text-7xl text-white">
+        My Portfolio
+      </div>
     </div>
   )
 }
